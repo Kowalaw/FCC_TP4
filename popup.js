a312b0343aabe993b4f440b51a9adc229c9626d2
Kowalaw <140055290+Kowalaw@users.noreply.github.com>
2023-08-11 12:08:14 +0200

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,10 +1,12 @@
 let popup = document.getElementById('popup')
+let fondPop = document.getElementById('fondPopUp')
 
 function popupOn(){
     let openPop = document.getElementsByClassName("com")
     for(let i=0;openPop.length;i++){
         openPop[i].addEventListener("click",()=>{
             popup.classList.remove('popupNone')
+            fondPop.classList.remove('popupNone')
         })
     }
 }
@@ -18,7 +20,12 @@ function popupOff(){
     let closePop = document.getElementById('valider')
     closePop.addEventListener('click',()=>{
         popup.classList.add('popupNone')
-        console.log('clik')
+        fondPop.classList.add('popupNone')
+    })
+    let closePopup = document.getElementById('closePopup')
+    closePopup.addEventListener("click",()=>{
+        popup.classList.add('popupNone')
+        fondPop.classList.add('popupNone')
     })
 }
 
